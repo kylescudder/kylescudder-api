237dc30d39aabd9c5dfb3e75c481c1b24b136ae1
Kyle Scudder <kyle.s@paloma.co.uk>
2021-05-06 08:08:57 +0100

    Added smtp email route

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const bodyParser = require("body-parser");
 const FormData = require("form-data");
 const fetch = require("node-fetch");
 const dotenv = require("dotenv");
-
+const MJ_APIKEY_PUBLIC = process.env.MJ_APIKEY_PUBLIC
+const MJ_APIKEY_PRIVATE = process.env.MJ_APIKEY_PRIVATE
 
 const app = express();
 
@@ -17,7 +18,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("/authenticate", (req, res) => {
+app.post("/GHIssueBeautifier/authenticate", (req, res) => {
   const { code } = req.body;
   dotenv.config();
   const data = new FormData();
@@ -51,7 +52,8 @@ app.post("/authenticate", (req, res) => {
     });
 });
 
-app.post("/issues", (req, res) => {
+app.post("/GHIssueBeautifier/issues", (req, res) => {
+  console.log(req.body)
   const { repo } = req.body;
   dotenv.config();
   // Request to exchange code for an access token
@@ -69,6 +71,37 @@ app.post("/issues", (req, res) => {
       return res.status(400).json(error);
     });
 });
+app.post('Email/contact', (req, res) => {
+  const name = req.body.name
+  const email = req.body.email
+  const message = req.body.message
+  const mailjet = require('node-mailjet')
+    .connect(MJ_APIKEY_PUBLIC, MJ_APIKEY_PRIVATE)
+  const request = mailjet
+    .post('send', { version: 'v3.1' })
+    .request({
+      Messages: [{
+        From: {
+          Email: email,
+          Name: name
+        },
+        To: [{
+          Email: 'scud1997@gmail.com',
+          Name: 'Kyle Scudder'
+        }],
+        Subject: 'Contact Form Message',
+        TextPart: `Name: ${name} Email: ${email} Message: ${message}`,
+        HTMLPart: `<p>Name: ${name}</p><p>Email: ${email}</p><p>Message: ${message}</p>`
+      }]
+    })
+  request
+    .then(() => {
+      res.json({ status: 'sent' })
+    })
+    .catch(() => {
+      res.json({ status: 'failed' })
+    })
+})
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
